c5ad42d1a4f0c124686cb474724da4d5737c486a
amcg <andy.h.mcgregor@gmail.com>
2018-03-20 13:31:30 +0000

    Initial Commit

--- src/main/java/com/amcg/hateos/controller/GreetingController.java
+++ src/main/java/com/amcg/hateos/controller/GreetingController.java
@@ -28,7 +28,12 @@ public class GreetingController {
         return new ResponseEntity<>(greeting, HttpStatus.OK);
     }
 
-    
+
+    /**
+     *
+     * @param name
+     * @return
+     */
     @PutMapping("/greeting")
     public HttpEntity<Greeting> putGreeting(
             @RequestParam(value = "name", required = false, defaultValue = "World") String name) {
