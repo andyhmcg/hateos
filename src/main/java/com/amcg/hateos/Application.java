e5d07b652d55a841697422716df83e9f0bc07995
amcg <andy.h.mcgregor@gmail.com>
2018-03-22 12:36:56 +0000

    Add trigger

--- src/main/java/com/amcg/hateos/Application.java
+++ src/main/java/com/amcg/hateos/Application.java
@@ -9,10 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class Application {
 
-    /**
-     * 
-     * @param args
-     */
+   
 
     public static void main(String[] args){
 
